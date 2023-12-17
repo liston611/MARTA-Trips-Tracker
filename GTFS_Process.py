22e939931c2edf5924b5594f009204bf0a6b9f21
liston611 <liston611@gmail.com>
2023-12-17 00:35:46 -0500

    added jupyter as well as requirements.txt. Played around with gtfs_functions a little.

--- GTFS_Process.py
+++ GTFS_Process.py
@@ -2,8 +2,10 @@ from gtfs_functions import Feed
 
 #pull GTFS
 gtfs_path = 'https://www.itsmarta.com/google_transit_feed/google_transit.zip'
-feed = Feed(gtfs_path)
+feed = Feed(gtfs_path, time_windows=[0, 6, 10, 12, 16, 19, 24])
 
+routes = feed.routes
+routes.head(2)
 
 #Find planned trips for current time from GTFS (calendar.txt, trips.txt)
 
