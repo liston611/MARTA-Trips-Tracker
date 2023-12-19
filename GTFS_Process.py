38a98e7f3d8ef28161dfab8e5b31e9c2be8696a1
liston611 <liston611@gmail.com>
2023-12-18 23:06:10 -0500

    Updated the jupyter so that now there is a table of all ongoing trips for current time.

--- GTFS_Process.py
+++ GTFS_Process.py
@@ -1,16 +1,39 @@
-from gtfs_functions import Feed
+# Install packages
+import pandas as pd
+import requests
+import zipfile
+import io
 
 #pull GTFS
 gtfs_path = 'https://www.itsmarta.com/google_transit_feed/google_transit.zip'
-feed = Feed(gtfs_path, time_windows=[0, 6, 10, 12, 16, 19, 24])
 
-routes = feed.routes
-routes.head(2)
+# Create functions to access GTFS and store files in memory
+def load_gtfs_data(
+        url,
+        files = [
+            'stops.txt', 'routes.txt', 'trips.txt', 'stop_times.txt',
+            'calendar.txt', 'calendar_dates.txt', 'shapes.txt'
+        ]):
+    """
+    Load GTFS data from a URL and convert each required .txt file into a pandas DataFrame, all in memory.
+    """
+    response = requests.get('https://www.itsmarta.com/google_transit_feed/google_transit.zip')
+    zip_file = zipfile.ZipFile(io.BytesIO(response.content))
 
-#Find planned trips for current time from GTFS (calendar.txt, trips.txt)
+    # Define the required GTFS files
+    required_files = files
 
-#Pull actual trips  for current time from GTFS-RT (vehiclepositions.pb)
+    # Load each file into a DataFrame
+    dataframes = {}
+    for file in required_files:
+        try:
+            with zip_file.open(file) as f:
+                df = pd.read_csv(f)
+                dataframes[file] = df
+        except KeyError:
+            print(f"{file} not found in the GTFS feed.")
 
-#Compare planned with actual, build DF with missed trips [[trip, route]]
+    return dataframes
 
-#return table of missed trips
+feed = load_gtfs_data(gtfs_path)
+x = 5
